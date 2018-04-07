72c183ae3251f9f428d9c48fcda6216425d8d394
litefeel <litefeel@gmail.com>
2018-04-07 17:49:47 +0800

    update

--- test2/dest/StaticData.lua
+++ test2/dest/StaticData.lua
@@ -2,19 +2,23 @@ local CURRENT_PACKAGE = ...
 CURRENT_PACKAGE = string.sub(CURRENT_PACKAGE, 1, -12)
 local manager = {}
 
-manager.HeroStaticVo_Data = require(CURRENT_PACKAGE..".HeroStaticVo_Data")
-function manager.getHeroStaticVo(id)
-    return manager.HeroStaticVo_Data[tostring(id)]
+function manager.getDungeon(id)
+    if not manager.Dungeon_Data then
+        manager.Dungeon_Data = require(CURRENT_PACKAGE..".Dungeon_Data")
+    end
+    return manager.Dungeon_Data[id]
 end
-manager.EquipStaticVo_Data = require(CURRENT_PACKAGE..".EquipStaticVo_Data")
-function manager.getEquipStaticVo(id)
-    return manager.EquipStaticVo_Data[tostring(id)]
+function manager.getItem(id)
+    if not manager.Item_Data then
+        manager.Item_Data = require(CURRENT_PACKAGE..".Item_Data")
+    end
+    return manager.Item_Data[id]
 end
 
 
 function manager.clear()
-_G.package[CURRENT_PACKAGE..".HeroStaticVo_Data"]=nil
-_G.package[CURRENT_PACKAGE..".EquipStaticVo_Data"]=nil
+_G.package[CURRENT_PACKAGE..".Dungeon_Data"]=nil
+_G.package[CURRENT_PACKAGE..".Item_Data"]=nil
 end
 
 return manager
